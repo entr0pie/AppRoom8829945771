49bfadc745ee0f1eb85527686472d5fd8b141ee2
entr0pie <caiohporcel@gmail.com>
2024-11-24 13:55:14 -0300

    feat: adiciona entity de Item ao projeto

--- app/src/main/java/br/edu/up/rgm8829945771/data/Item.kt
+++ app/src/main/java/br/edu/up/rgm8829945771/data/Item.kt
@@ -1,6 +1,11 @@
 package br.edu.up.rgm8829945771.data
 
-class Item(
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "ITEMS")
+data class Item(
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val name: String,
     val price: Double,
